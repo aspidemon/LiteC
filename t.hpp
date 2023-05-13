7aa7c5cf115283cf1d6884815dd4a4bd1d800469
by17s <127589735+by17s@users.noreply.github.com>
2023-05-13 21:39:12 +0600

    Update t.hpp

--- t.hpp
+++ t.hpp
@@ -7,7 +7,6 @@
 #include <fstream>
 #include <map>
 #include "exp.hpp"
-#include "ast.hpp"
 
 #define _and &&
 #define _or ||
@@ -343,66 +342,4 @@ int run(string data) {
         return -1;
     }
     //return i_run(datt, dat);
-}
-
-class Parser {
-private:
-    vector<tokens> datt;
-    int pos = 0;
-
-public:
-    Parser(vector<tokens> datt) {
-        this->datt = datt;
-    };
-
-    template <typename T>
-    T match(tokens tok) {
-        if (this->pos < this->datt.length()) {
-            tokens currentt = this->datt[this->pos];
-            for (tokens t : tok) {
-                if (tok == t) {
-                    this->pos += 1;
-                    return currentt;
-                }
-            }
-        }
-        return 0;
-    };
-
-    tokens require(tokens tok) {
-        tokens token = this->match(tokens);
-        //сюды вызов ошибки
-        return token;
-    };
-
-    ExpressionNode parseExpression() {
-        if (this->match(tokens::WORD) == 0) {
-            printNode = parsePrint();
-            return printNode;
-        }
-        this->pos -= 1;
-        //varNode = parseVarOrNumber();
-        //assignOperator = this->match(tokens::OPERATION);
-
-    };
-
-    ExpressionNode parse() {
-        StatementsNode root();
-        while (this->pos < this->datt.length()) {
-            ExpressionNode stringNode = this->parseExpression();
-            this->require(tokens::END);
-            root.addNode(stringNode);
-        }
-        return root;
-    };
-
-};
-
-int mainggg() {
-    int retval = run("int var = 0;\npb int main(\"ggg\") {\nv.call(\"kkkk\", =999);\n}");
-    if (retval == -2) {
-        expnow(GET_VARIABLE_NAME(exceptions::UnknownError), -1, -1, 'n');
-        return -1;
-    }
-    return retval;
 }
